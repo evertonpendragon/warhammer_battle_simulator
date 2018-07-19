8d13647694a43ca97fe30e05997f759492bd95bd
Everton Matos <everton.de.rezende@gmail.com>
2018-07-18 23:07:05 -0300

    dev

--- warhammer_battle_simulator.py
+++ warhammer_battle_simulator.py
@@ -1,1 +1,8 @@
-print ('oi')
+for p in range(100):
+    print("Partida", p)
+
+    unidades=[{"unidade1":{"nome":"Primaris Intercessors" , "ws":3}}]
+
+    for turno in range (6):
+        print("\tTurno", turno)
+        print(unidades[0]["unidade1"]["ws"])
