81be39b2eaeadc36d067ca5f7ec4b0baf7f00d88
Everton Matos <everton.de.rezende@gmail.com>
2018-07-18 23:40:11 -0300

    dev1

--- cria_arquivo_config.py
+++ cria_arquivo_config.py
@@ -1,27 +1,30 @@
 import configparser
 config = configparser.ConfigParser()
-config['Primaris Intercessor Squad'] = {'role': {"Intercessor":{"M"       :6      ,
-                                                                "WS"      :3      ,
-                                                                "BS"      :3      ,
-                                                                "S"       :4      ,
-                                                                "T"       :4      ,
-                                                                "W"       :2      ,
-                                                                "A"       :2      ,
-                                                                "Ld"      :7      ,
-                                                                "Save"    :3      ,
-                                                                "Ref"     :0
+config['Primaris Intercessor Squad'] = {'role': {"Intercessor":{"M"       :6,
+                                                                "WS"      :3,
+                                                                "BS"      :3,
+                                                                "S"       :4,
+                                                                "T"       :4,
+                                                                "W"       :2,
+                                                                "A"       :2,
+                                                                "Ld"      :7,
+                                                                "Save"    :3,
+                                                                "Ref"     :0,
+                                                                "Qtd"     :4
                                                                  }
-                                                 ,"Intercessor Sergeant":{"M"       :6      ,
-                                                                "WS"      :3      ,
-                                                                "BS"      :3      ,
-                                                                "S"       :4      ,
-                                                                "T"       :4      ,
-                                                                "W"       :2      ,
-                                                                "A"       :2      ,
-                                                                "Ld"      :8      ,
-                                                                "Save"    :3      ,
-                                                                "Ref"     :0
-                                                                 }
+                                                 ,"Intercessor Sergeant":{  "M"       :6,
+                                                                            "WS"      :3,
+                                                                            "BS"      :3,
+                                                                            "S"       :4,
+                                                                            "T"       :4,
+                                                                            "W"       :2,
+                                                                            "A"       :2,
+                                                                            "Ld"      :8,
+                                                                            "Save"    :3,
+                                                                            "Ref"     :0,
+                                                                            "Qtd"     :1,
+                                                                            "Prioridade":1
+                                                                             }
                                                  }
                                         }
 
--- warhammer_battle_simulator.py
+++ warhammer_battle_simulator.py
@@ -12,10 +12,6 @@ for p in range(100):
 
 
 
-
-
-
-
 """ 
 M
 WS
